e62dcf3871483a5f64f03c2f518c4517749f114f
Sion1225 <visiopo44@naver.com>
2024-10-15 16:15:25 +0900

    extract_fix

--- extract_score.py
+++ extract_score.py
@@ -65,9 +65,13 @@ def main(
             f_softmax.write(f'Token Length: {len(tokens)}\n')
             f_softmax.write(f'Index of Tokens: {tokens}\n')
             f_softmax.write(f'Tokens: {tokenizer.decode(tokens)}\n')
+
+            f_softmax_wo_bos_eos.write(f'Prompt: {prompt}\n')
+            f_softmax_wo_bos_eos.write(f'Token Length: {len(tokens)}\n')
+            f_softmax_wo_bos_eos.write(f'Index of Tokens: {tokens}\n')
+            f_softmax_wo_bos_eos.write(f'Tokens: {tokenizer.decode(tokens)}\n')
             
             truncated_scores = scores[:, :len(tokens)]
-
             for head, score_vector in enumerate(truncated_scores):
                 f.write(f'Head {head} Attention Scores: {score_vector.tolist()}\n')
             f.write('\n')
@@ -77,9 +81,10 @@ def main(
                 f_softmax.write(f'Head {head} Attention Scores: {softmax_score_vector.tolist()}\n')
             f_softmax.write('\n')
 
+            truncated_scores = scores[:, 1:len(tokens)-1]
             for head, score_vector in enumerate(truncated_scores):
                 softmax_score_vector = score_vector.softmax(dim=-1)
-                f_softmax_wo_bos_eos.write(f'Head {head} Attention Scores: {softmax_score_vector[1:-1].tolist()}\n')
+                f_softmax_wo_bos_eos.write(f'Head {head} Attention Scores: {softmax_score_vector.tolist()}\n')
             f_softmax_wo_bos_eos.write('\n')
 
 
