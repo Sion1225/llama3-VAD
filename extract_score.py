5d065f49edfda1a109447869199789cd349b87f4
Sion1225 <visiopo44@naver.com>
2025-06-10 21:11:42 +0900

    bug fix

--- extract_score.py
+++ extract_score.py
@@ -101,7 +101,6 @@ def main(
     elif target_data == "empathetic_dialogue":
         count = 0
         batch_prompts: List[str] = []
-        batch_prompts_tokens = []
         with open(EMPATHETIC_DIALOGUE_PATH, 'r', encoding='utf-8') as f:
             for line in f:
                 batch_prompts.append(json.loads(line))
@@ -112,12 +111,11 @@ def main(
                         for utter in dialog:
                             utter["content"] = remove_soft_hyphen(utter["content"])
 
-                    batch_attention_scores, prompt_tokens = generator.extract_from_dialog_prompts(batch_prompts)
+                    batch_attention_scores, batch_prompts_tokens = generator.extract_from_dialog_prompts(batch_prompts)
 
                     output_attention_scores(output_path, tokenizer_path, batch_prompts, batch_prompts_tokens, batch_attention_scores)
 
                     batch_prompts = []
-                    batch_prompts_tokens = []
                     count = 0
             
             # 파일을 다 읽어오고 난 시점에서도 batch_prompts가 남아있을것이기에 처리를하고 출력까지 해야함.
@@ -126,7 +124,7 @@ def main(
                     for utter in dialog:
                         utter["content"] = remove_soft_hyphen(utter["content"])
 
-                batch_attention_scores, prompt_tokens = generator.extract_from_dialog_prompts(batch_prompts)
+                batch_attention_scores, batch_prompts_tokens = generator.extract_from_dialog_prompts(batch_prompts)
 
                 output_attention_scores(output_path, tokenizer_path, batch_prompts, batch_prompts_tokens, batch_attention_scores)
     else:
